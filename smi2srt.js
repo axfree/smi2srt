115d3dd230ae2ac50c141d711ebe75320ebba156
axfree <axfree@live.com>
2018-06-06 04:35:29 +0900

    Add Chinese.

--- smi2srt.js
+++ smi2srt.js
@@ -48,7 +48,7 @@ argv.args.forEach(f => {
     }
 
     files.forEach(file => {
-        var fileMatch = file.match(/^(.*?)(?:\.(en|eng|ko|kor|ja|jap))?(?:\.(smi|smil|srt|ass))$/i);
+        var fileMatch = file.match(/^(.*?)(?:\.(en|eng|ko|kor|ja|jap|zh-cn|chs|zh-tw|cht))?(?:\.(smi|smil|srt|ass))$/i);
         if (!fileMatch)
             return;
         var baseFile = fileMatch[1];
